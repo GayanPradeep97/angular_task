ed0bc25a9b0d8087aa2a15cdc6135dcb691d3300
GayanPradeep97 <gayanpradeep0212@gmail.com>
2023-03-08 21:51:37 -0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.componnt';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
